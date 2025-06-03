e23f1b6deb8bfa5e76974b06bad9170af0ca8dd0
eduard0915 <edd.alvis@gmail.com>
2025-06-02 23:31:43 -0500

    Se realiza avance en app training y competence

--- core/company/models.py
+++ core/company/models.py
@@ -1,3 +1,100 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from core.validators import validator_file_image
+from luka.settings import MEDIA_URL, STATIC_URL
+
+
+# Empresa
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    company_name = models.CharField(default='Nombre Empresa', max_length=120, verbose_name='Nombre Empresa')
+    company_logo = models.ImageField(
+        upload_to='company', null=True, blank=True, verbose_name='Logo', validators=[validator_file_image])
+    company_nit = models.CharField(default='0000000000-0', max_length=20, verbose_name='NIT')
+    company_address = models.CharField(default='Direccion', max_length=60, verbose_name='Dirección')
+    company_city = models.CharField(default='Ciudad', max_length=60, verbose_name='Ciudad')
+    company_country = models.CharField(default='Pais', max_length=60, verbose_name='Pais')
+
+    def __str__(self):
+        return str(self.company_name)
+
+    def get_logo(self):
+        if self.company_logo:
+            return '{}{}'.format(MEDIA_URL, self.company_logo)
+        return '{}{}'.format(STATIC_URL, 'img/empty.png')
+
+    class Meta:
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
+        db_table = 'Company'
+
+
+# Plantas
+class Site(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    site_name = models.CharField(max_length=120, verbose_name='Planta')
+    site_address = models.CharField(max_length=60, verbose_name='Dirección')
+    site_city = models.CharField(max_length=60, verbose_name='Ciudad')
+    site_country = models.CharField(max_length=60, verbose_name='País')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='')
+
+    def __str__(self):
+        return str(self.site_name)
+
+    class Meta:
+        verbose_name = 'Site'
+        verbose_name_plural = 'Sites'
+        db_table = 'Site'
+
+
+# Procesos
+class Process(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    process_name = models.CharField(max_length=120, verbose_name='Proceso')
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, verbose_name='Planta')
+    enable_process = models.BooleanField(default=True, verbose_name='Habilitado')
+
+    def __str__(self):
+        return str(self.process_name)
+
+    class Meta:
+        verbose_name = 'Process'
+        verbose_name_plural = 'Processes'
+        db_table = 'Process'
+
+
+# Etapas
+class Stage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    stage_name = models.CharField(max_length=100, verbose_name='Etapa')
+    stage_code = models.CharField(max_length=20, verbose_name='Codigo/Abreviación')
+    process = models.ForeignKey(Process, on_delete=models.CASCADE, verbose_name='Proceso')
+    enable_stage = models.BooleanField(default=True, verbose_name='Habilitado')
+
+    def __str__(self):
+        return str(self.stage_name)
+
+    class Meta:
+        verbose_name = 'Stage'
+        verbose_name_plural = 'Stages'
+        db_table = 'Stage'
+
+
+# Puntos de Muestreo
+class SamplePoint(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    sample_point_name = models.CharField(max_length=100, verbose_name='Punto de Muestreo')
+    sample_point_code = models.CharField(max_length=30, verbose_name='Codigo/Abreviación')
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE, verbose_name='Etapa')
+    enable_point = models.BooleanField(default=True, verbose_name='Habilitado')
+    slug = models.SlugField(unique=True, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.sample_point_name)
+
+    class Meta:
+        verbose_name = 'SamplePoint'
+        verbose_name_plural = 'SamplePoints'
+        db_table = 'SamplePoint'
--- core/user/forms.py
+++ core/user/forms.py
@@ -1,7 +1,6 @@
 from django.forms import *
 
-from core.user.models import User
-
+from core.user.models import *
 
 SELECT = [(True, 'Si'), (False, 'No')]
 
@@ -11,6 +10,7 @@ class UserForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['groups'].label = 'Perfil'
+        self.fields['email'].label = 'E-mail'
         for form in self.visible_fields():
             form.field.widget.attrs['autocomplete'] = 'off'
 
@@ -59,6 +59,83 @@ class UserForm(ModelForm):
                 u.groups.clear()
                 for g in self.cleaned_data['groups']:
                     u.groups.add(g)
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return data
+
+
+# Edición de usuario por administrador
+class UserUpdateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['groups'].label = 'Perfil'
+        for form in self.visible_fields():
+            form.field.widget.attrs['autocomplete'] = 'off'
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'cargo', 'cellphone', 'cedula', 'username', 'groups']
+        widgets = {
+            'first_name': TextInput(attrs={'class': 'form-control', 'required': True}),
+            'last_name': TextInput(attrs={'class': 'form-control', 'required': True}),
+            'email': EmailInput(attrs={'class': 'form-control', 'required': True}),
+            'cargo': TextInput(attrs={'class': 'form-control'}),
+            'cellphone': TextInput(attrs={'class': 'form-control'}),
+            'cedula': TextInput(attrs={'class': 'form-control'}),
+            'username': TextInput(attrs={'class': 'form-control', 'readonly': True}),
+            'groups': SelectMultiple(attrs={'class': 'form-control', 'required': True}),
+            # 'photo': FileInput(attrs={'class': 'form-control-file'})
+
+        }
+        exclude = ['user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_staff']
+
+        help_texts = {
+            'groups': 'Seleccione perfil del usuario',
+            # 'username': ''
+        }
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                data = form.save()
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return data
+
+
+# Creación de capacitación
+class TrainingForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        # self.user = kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+        for form in self.visible_fields():
+            form.field.widget.attrs['autocomplete'] = 'off'
+
+    class Meta:
+        model = Training
+        fields = ['description_training', 'training_by', 'date_training', 'support_training']
+        widgets = {
+            'description_training': TextInput(attrs={'class': 'form-control', 'required': True}),
+            'training_by': TextInput(attrs={'class': 'form-control', 'required': True}),
+            'support_training': FileInput(attrs={'class': 'form-control-file'}),
+            'date_training': DateInput(attrs={'class': 'form-control'})
+
+        }
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                data = form.save(commit=False)
+                # data.user_id = self.user.id
+                data.save()
             else:
                 data['error'] = form.errors
         except Exception as e:
