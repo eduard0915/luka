95fb77e81b9f8d7b8be5950eb85941a2831a65d8
eduard0915 <edd.alvis@gmail.com>
2025-09-21 17:06:42 -0500

    Vista list de usuarios, finalizada

--- core/home/views.py
+++ core/home/views.py
@@ -7,5 +7,5 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Luka Lims'
+        context['title'] = 'Quality Lims'
         return context
