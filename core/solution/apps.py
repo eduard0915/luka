88f46ad2f27c3cf9570e36657f1434b9dbdf3e03
eduard0915 <edd.alvis@gmail.com>
2025-10-29 07:26:25 -0500

    Historial de moviemientos de reactivos

--- core/solution/apps.py
+++ core/solution/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class SolutionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core.solution'
+
+    def ready(self):
+        # Importing signals to ensure they are registered when the app is ready
+        try:
+            import core.signals  # noqa: F401
+        except ImportError as e:
+            print(f"Error importing signals: {e}")
