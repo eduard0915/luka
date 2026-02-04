8c2b6488c54dcf2c30deca916fd001a907a2058e
eduard0915 <edd.alvis@gmail.com>
2026-02-04 15:55:23 -0500

    Avance calcule sample concentracion

--- core/sampling/forms.py
+++ core/sampling/forms.py
@@ -1,27 +1,29 @@
-from crum import get_current_user
-from django.core.exceptions import ValidationError
 from django.forms import ModelForm, TextInput, Select, TimeInput, DateTimeInput
+from django.core.exceptions import ValidationError
 from django.utils import timezone
-
-from core.product.models import SamplePoint
+from crum import get_current_user
 from core.sampling.models import SamplingGroup, SamplingProcess, SamplingAnalysisProcessing
+from core.product.models import SamplePoint
 from core.solution.models import SolutionStd
-
+from core.analytical_method.models import AnalyticalMethodCalculate
 
 TYPE_SAMPLING = [('En Proceso', 'En Proceso'), ('Producto Terminado', 'Producto Terminado')]
 
-
-# Registro de Procesamiento de Análisis
 class SamplingAnalysisProcessingForm(ModelForm):
     def __init__(self, *args, **kwargs):
         self.analysis = kwargs.pop('analysis')
         super().__init__(*args, **kwargs)
-        # Filtrar soluciones estándar asociadas al método de análisis y que tengan stock
+        calcs = AnalyticalMethodCalculate.objects.select_related('analytical_method').filter(analytical_method_id=self.analysis.analytical_method.id)
+        calc_con_label = calcs.exclude(sample_quantity__in=[None, '', False]).first()
+
         std_bases = self.analysis.analytical_method.analyticalmethodsolutionstd_set.values_list('solution_std_id', flat=True)
         self.fields['standard_solution'].queryset = SolutionStd.objects.select_related('solute_std').filter(
             solution_std_base_id__in=std_bases,
             preparation_confirmed=True,
             quantity_solution_std__gt=0)
+        
+        if calc_con_label and calc_con_label.sample_quantity:
+            self.fields['quantity_sample'].label = str(calc_con_label.sample_quantity)
 
         for form in self.visible_fields():
             form.field.widget.attrs['autocomplete'] = 'off'
@@ -38,32 +40,61 @@ class SamplingAnalysisProcessingForm(ModelForm):
         model = SamplingAnalysisProcessing
         fields = ['standard_solution', 'quantity_standard', 'quantity_sample']
         widgets = {
-            'standard_solution': Select(attrs={
-                'class': 'form-control select2',
-                'required': True,
-                'style': 'width: 100%'
-            }),
+            'standard_solution': Select(attrs={'class': 'form-control select2', 'required': True, 'style': 'width: 100%'}),
             'quantity_standard': TextInput(attrs={'class': 'form-control', 'required': True}),
             'quantity_sample': TextInput(attrs={'class': 'form-control', 'required': True}),
         }
 
     def save(self, commit=True):
         user = get_current_user()
+        var_num = AnalyticalMethodCalculate.objects.select_related('analytical_method').filter(analytical_method_id=self.analysis.analytical_method.id, position='Numerador')
+        var_den = AnalyticalMethodCalculate.objects.select_related('analytical_method').filter(analytical_method_id=self.analysis.analytical_method.id, position='Denominador')
+
         try:
             instance = super().save(commit=False)
             instance.sample_analysis_id = self.analysis.id
             instance.analyzed_by_id = user.id
             instance.analyzed_date = timezone.localtime()
 
-            # Cálculo de la concentración
+            if instance.quantity_standard is None or instance.quantity_sample is None or instance.standard_solution.concentration_std is None:
+                raise ValidationError(
+                    "Los campos cantidad estándar, cantidad de muestra y concentración del estándar son obligatorios")
+
             qty_std = float(instance.quantity_standard)
             qty_sample = float(instance.quantity_sample)
-            conc_std = instance.standard_solution.concentration_std
-
             cifras_sign = instance.sample_analysis.analytical_method.sig_figs_result
-            
+            # conc_std = float(instance.standard_solution.concentration_std)
+
             if qty_sample > 0:
-                instance.concentration_sample = round((qty_std * conc_std) / qty_sample, cifras_sign)
+
+                factor_num = 1
+                sample = 1
+
+                for num in var_num:
+                    # Solo multiplicar si el factor no es None
+                    if num.factor is not None:
+                        factor_num *= float(num.factor)
+
+                    if num.sample_quantity is not None:
+                        sample = float(qty_sample)
+
+                print(qty_std, qty_sample, factor_num)
+                numerator = round((qty_std * factor_num * sample), cifras_sign)
+
+                factor_den = 1
+
+                for den in var_den:
+                    # Solo multiplicar si el factor no es None
+                    if den.factor is not None:
+                        factor_den *= float(den.factor)
+
+                # El cálculo del denominador debe ir FUERA del bucle
+                denominator = round((qty_std * factor_den * qty_sample), cifras_sign)
+
+                print(factor_num, factor_den)
+                print(numerator, denominator)
+
+                instance.concentration_sample = round((numerator / denominator), cifras_sign)
             else:
                 instance.concentration_sample = 0
 
@@ -73,19 +104,13 @@ class SamplingAnalysisProcessingForm(ModelForm):
         except Exception as e:
             raise ValidationError({'error': str(e)})
 
-
-# Creación de Grupos de Muestreo
 class SamplingGroupForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['sampling_point'].queryset = SamplePoint.objects.filter(enable_point=True, sample_frequency__isnull=False)
         for form in self.visible_fields():
             form.field.widget.attrs['autocomplete'] = 'off'
-
-        col_classes = {
-            'sampling_point': 'col-md-6'
-        }
-
+        col_classes = {'sampling_point': 'col-md-6'}
         for field_name, field in self.fields.items():
             field.col_class = col_classes.get(field_name, 'col-md-3')
 
@@ -110,22 +135,13 @@ class SamplingGroupForm(ModelForm):
             data['error'] = str(e)
         return data
 
-
-# Proceso de Muestreo
 class SamplingProcessForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for form in self.visible_fields():
-            self.fields['point_sampling'].queryset = SamplePoint.objects.filter(
-                enable_point=True, sample_type='Producto Terminado', sample_frequency__isnull=True)
+            self.fields['point_sampling'].queryset = SamplePoint.objects.filter(enable_point=True, sample_type='Producto Terminado', sample_frequency__isnull=True)
             form.field.widget.attrs['autocomplete'] = 'off'
-
-        col_classes = {
-            'point_sampling': 'col-md-4',
-            'group_sampling': 'col-md-4',
-            'batch_number': 'col-md-2',
-        }
-
+        col_classes = {'point_sampling': 'col-md-4', 'group_sampling': 'col-md-4', 'batch_number': 'col-md-2'}
         for field_name, field in self.fields.items():
             field.col_class = col_classes.get(field_name, 'col-md-3')
 
@@ -144,15 +160,10 @@ class SamplingProcessForm(ModelForm):
         cleaned_data = super().clean()
         group_sampling = cleaned_data.get('group_sampling')
         point_sampling = cleaned_data.get('point_sampling')
-
-        # Si está diligenciado group_sampling, asegurar que point_sampling sea None
         if group_sampling:
             cleaned_data['point_sampling'] = None
-
-        # Si está diligenciado point_sampling, asegurar que group_sampling sea None
         if point_sampling:
             cleaned_data['group_sampling'] = None
-
         return cleaned_data
 
     def save(self, commit=True):
@@ -171,8 +182,6 @@ class SamplingProcessForm(ModelForm):
             data['error'] = str(e)
         return data
 
-
-# Formulario para actualizar la foto de la muestra
 class SamplingProcessImageForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -182,9 +191,7 @@ class SamplingProcessImageForm(ModelForm):
     class Meta:
         model = SamplingProcess
         fields = ['image_sample']
-        widgets = {
-            'image_sample': TextInput(attrs={'class': 'form-control', 'type': 'file'}),
-        }
+        widgets = {'image_sample': TextInput(attrs={'class': 'form-control', 'type': 'file'})}
 
     def save(self, commit=True):
         data = {}
@@ -198,8 +205,6 @@ class SamplingProcessImageForm(ModelForm):
             data['error'] = str(e)
         return data
 
-
-# Confirmación de la Muestra
 class SamplingProcessConfirmedForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -209,9 +214,7 @@ class SamplingProcessConfirmedForm(ModelForm):
     class Meta:
         model = SamplingProcess
         fields = ['image_sample']
-        widgets = {
-            'image_sample': TextInput(attrs={'class': 'form-control', 'type': 'file'}),
-        }
+        widgets = {'image_sample': TextInput(attrs={'class': 'form-control', 'type': 'file'})}
 
     def save(self, commit=True):
         data = {}
@@ -229,8 +232,6 @@ class SamplingProcessConfirmedForm(ModelForm):
             data['error'] = str(e)
         return data
 
-
-# Cambio de Estado de la Muestra a En Proceso
 class SamplingProcessInProcessForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -241,9 +242,7 @@ class SamplingProcessInProcessForm(ModelForm):
     class Meta:
         model = SamplingProcess
         fields = ['status_sampling']
-        widgets = {
-            'status_sampling': TextInput(attrs={'class': 'form-control', 'hidden': 'true'}),
-        }
+        widgets = {'status_sampling': TextInput(attrs={'class': 'form-control', 'hidden': 'true'})}
 
     def save(self, commit=True):
         data = {}
