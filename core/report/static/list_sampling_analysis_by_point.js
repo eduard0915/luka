565a077a493d2bd9aa0b314fbb3164ed4f6c2cd5
eduard0915 <edd.alvis@gmail.com>
2026-06-20 17:22:50 -0500

    Completitud de reportes en en tablas, graficos y excel

--- core/report/static/list_sampling_analysis_by_point.js
+++ core/report/static/list_sampling_analysis_by_point.js
@@ -1,5 +1,5 @@
 var tblAnalysis;
-var select_analytical_method;
+var select_sample_point;
 
 function initTable(json) {
     var table = $('#data');
@@ -32,7 +32,7 @@ function initTable(json) {
         columnDefs: [
             {
                 targets: ['_all'],
-                class: 'text-center',
+                class: 'text-center align-middle',
                 defaultContent: '-'
             }
         ],
@@ -44,9 +44,11 @@ function initTable(json) {
 
 function loadData() {
     var product = $('#id_product').val();
-    var analytical_method = $('#id_analytical_method').val();
+    var sample_point = $('#id_sample_point').val();
+    var start_date = $('#id_start_date').val();
+    var end_date = $('#id_end_date').val();
     
-    if (!analytical_method) {
+    if (!sample_point) {
         initTable({columns: [], data: []});
         return false;
     }
@@ -57,7 +59,9 @@ function loadData() {
         data: {
             'action': 'searchdata',
             'product': product,
-            'analytical_method': analytical_method
+            'sample_point': sample_point,
+            'start_date': start_date,
+            'end_date': end_date
         },
         dataType: 'json',
     }).done(function (json) {
@@ -72,13 +76,13 @@ function loadData() {
 }
 
 $(function () {
-    select_analytical_method = $('#id_analytical_method');
+    select_sample_point = $('#id_sample_point');
 
     $('#id_product').on('change', function () {
         var id = $(this).val();
         var options = '<option value="">---------</option>';
         if (id === '') {
-            select_analytical_method.html(options);
+            select_sample_point.html(options);
             loadData();
             return false;
         }
@@ -86,7 +90,7 @@ $(function () {
             url: window.location.pathname,
             type: 'POST',
             data: {
-                'action': 'search_analytical_method',
+                'action': 'search_sample_point',
                 'id': id
             },
             dataType: 'json',
@@ -101,13 +105,32 @@ $(function () {
         }).fail(function (jqXHR, textStatus, errorThrown) {
             alert(textStatus + ': ' + errorThrown);
         }).always(function () {
-            select_analytical_method.html(options);
+            select_sample_point.html(options);
             loadData();
         });
     });
 
-    $('#id_analytical_method').on('change', function () {
+    $('#id_sample_point').on('change', function () {
         loadData();
+    });
+
+    $('#id_start_date, #id_end_date').on('change', function () {
+        loadData();
+    });
+
+    $('.btnExcel').on('click', function () {
+        var product = $('#id_product').val();
+        var sample_point = $('#id_sample_point').val();
+        var start_date = $('#id_start_date').val();
+        var end_date = $('#id_end_date').val();
+        
+        if (!sample_point) {
+            alert('Debe seleccionar un punto de muestreo');
+            return false;
+        }
+
+        var url = window.location.pathname + 'excel/?product=' + product + '&sample_point=' + sample_point + '&start_date=' + start_date + '&end_date=' + end_date;
+        window.open(url, '_blank');
     });
 
     // Carga inicial
--- core/report/urls.py
+++ core/report/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from core.report.views import SamplingAnalysisListView, SamplingAnalysisByPointListView, SamplingAnalysisChartView, \
-    SamplingAnalysisByPointExcelView, SamplingAnalysisProcessingListView, SamplingAnalysisProcessingExcelView
+    SamplingAnalysisByPointExcelView, SamplingAnalysisProcessingListView, SamplingAnalysisProcessingExcelView, SamplingAnalysisExcelView
 
 app_name = 'report'
 
 urlpatterns = [
     path('sampling/analysis/list/', SamplingAnalysisListView.as_view(), name='sampling_analysis_list'),
+    path('sampling/analysis/list/excel/', SamplingAnalysisExcelView.as_view(), name='sampling_analysis_excel'),
     path('sampling/analysis/list/point/', SamplingAnalysisByPointListView.as_view(), name='sampling_analysis_list_point'),
     path('sampling/analysis/chart/', SamplingAnalysisChartView.as_view(), name='sampling_analysis_chart'),
     path('sampling/analysis/list/point/excel/', SamplingAnalysisByPointExcelView.as_view(), name='sampling_analysis_list_point_excel'),
