776112e127c0eddb6ff38d87f9671c17efea67b8
eduard0915 <edd.alvis@gmail.com>
2026-08-31 14:52:54 -0500

    fix bug migrations solutions app

--- core/solution/migrations/0057_remove_standardization_solution_base_and_more.py
+++ core/solution/migrations/0057_remove_standardization_solution_base_and_more.py
@@ -18,7 +18,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='standardization',
             name='solution_stb_base_to_standardize',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='solution.solutionbase', verbose_name='Solución Base a Estándarizar'),
-            preserve_default=False,
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='solution.solutionbase', verbose_name='Solución Base a Estándarizar'),
         ),
     ]
